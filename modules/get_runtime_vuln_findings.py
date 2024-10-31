49d3cbce2d82ba9a0dfef030e487adab100c3d87
brian.morrissey <brian.morrissey@sysdig.com>
2024-10-31 10:05:21 -0400

    remove need to check for sbom

--- modules/get_runtime_vuln_findings.py
+++ modules/get_runtime_vuln_findings.py
@@ -50,55 +50,22 @@ def vulnRuntimeFindings(LOG, http_client, arg_secure_url_authority):
                 LOG.error(f"Details of missing key: {result}")
                 break
             
-            # Check for image in the report and skip downloading the SBOM if its not there to save time
-            with open('report', 'r') as file:
-                for line in file:
-                    if resourceId in line and resultClusterName and resultNamespaceName and resultContainerName and resultWorkloadName in line:
-                        foundInReport = True
-
-            if(foundInReport):
-                LOG.info(f"Getting SBOM for resultId: {resultId}, resourceId: {resourceId}")
-                url = f"https://{arg_secure_url_authority}/secure/vulnerability/v1beta1/sboms?assetId={resourceId}&assetType=container-image"
-                response = http_client.request(method="GET", url=url, redirect=True, timeout=3)
-
-                LOG.debug(f"Response status: {response.status}")
-
-                if response.status != 200:
-                    LOG.error(f"Error: {response.status} when trying to download SBOM {url}")
-                    break
- 
-                json_response_data = json.loads(response.data.decode()) 
-
-                # Extract the parameters name & version
-                components = json_response_data["components"]
-
-                for component in components:
-                    name = component["name"]
-                    version = component["version"]
-                    group = component.get("group", {})  
-
-                    #For java and others concat group and name from SBOM to match report format
-                    if(group):
-                        name = f"{group}:{name}"
-
-                    # Open the file report which is CSV delimited
-                    with open('report', mode='r') as file:
-                        csv_reader = csv.DictReader(file)
-                        
-                        # Iterate through each row in the CSV
-                        for row in csv_reader:
-                            # Match the fields
-                            if (row['Image ID'] == resourceId and
-                                row['Package name'] == name and
-                                row['Package version'] == version and
-                                row['K8S cluster name'] == resultClusterName and
-                                row['K8S namespace name'] == resultNamespaceName and
-                                row['K8S workload type'] == resultWorkloadType and
-                                row['K8S workload name'] == resultWorkloadName and
-                                row['K8S container name'] == resultContainerName):
-                                print(row['K8S cluster name'],"->",row['K8S namespace name'],"->",row['K8S workload type'],"->",row['K8S workload name'],"->",row['K8S container name'], row['Severity'], row['Package name'], row['Package version'], row['Vulnerability ID'])
-                                severityCount += 1
+            # Open the file report which is CSV delimited
+            with open('report', mode='r') as file:
+                csv_reader = csv.DictReader(file)
                 
-                if(severityCount):
-                    print(f"{RED}Total Critical: {severityCount}{RESET}")
-                    
+                # Iterate through each row in the CSV
+                for row in csv_reader:
+                    # Match the fields
+                    if (row['Image ID'] == resourceId and
+                        row['K8S cluster name'] == resultClusterName and
+                        row['K8S namespace name'] == resultNamespaceName and
+                        row['K8S workload type'] == resultWorkloadType and
+                        row['K8S workload name'] == resultWorkloadName and
+                        row['K8S container name'] == resultContainerName):
+                        print(row['K8S cluster name'],"->",row['K8S namespace name'],"->",row['K8S workload type'],"->",row['K8S workload name'],"->",row['K8S container name'], row['Severity'], row['Package name'], row['Package version'], row['Vulnerability ID'])
+                        severityCount += 1
+        
+            if(severityCount):
+                print(f"{RED}Total Critical: {severityCount}{RESET}")
+                
