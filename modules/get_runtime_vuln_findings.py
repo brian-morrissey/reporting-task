1081f4f518538161eaf6f9e5035aadc2f3d59554
brian.morrissey <brian.morrissey@sysdig.com>
2024-10-31 11:27:59 -0400

    fix output loop to avoid overwriting file when multiple pages

--- modules/get_runtime_vuln_findings.py
+++ modules/get_runtime_vuln_findings.py
@@ -24,26 +24,31 @@ def vulnRuntimeFindings(LOG, http_client, arg_secure_url_authority):
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     outputFilename = f'results_{timestamp}.csv'
 
+    # Add header to new report and overwrite if the file exists
+    with open('report', mode='r') as source_file, open(outputFilename, mode='w', newline='') as destination_file:
+        header = source_file.readline()
+        destination_file.write(header)
+        
     while nextPage:
         LOG.info("Getting Vuln->Findings->Runtime..")
         url = f"https://{arg_secure_url_authority}/api/scanning/runtime/v2/workflows/results?cursor={page}&filter&limit=100&order=desc&sort=runningVulnsBySev&zones"
         response = http_client.request(method="GET", url=url, redirect=True, timeout=3)
-        json_response_data = json.loads(response.data.decode())
         LOG.debug(f"Response status: {response.status}")
- 
+
+        if response.status != 200:
+            LOG.error(f"Error Quitting: {response.status}")
+            quit()
+
+        json_response_data = json.loads(response.data.decode())
+
         # Check if there is a next page and if not exit the loop
         page = json_response_data.get("page", {}).get("next", "")
 
-        # pull total count
+        # Get total count of runtime findings
         totalRuntimeFindings = json_response_data.get("page", {}).get("matched", "")
         
         if not page:
             nextPage = False
-
-        # Add header to new report and overwrite if the file exists
-        with open('report', mode='r') as source_file, open(outputFilename, mode='w', newline='') as destination_file:
-            header = source_file.readline()
-            destination_file.write(header)
             
         # Loop through all results
         for result in json_response_data["data"]:
@@ -85,12 +90,13 @@ def vulnRuntimeFindings(LOG, http_client, arg_secure_url_authority):
         
             if(severityCount):
                 print(f"{RED}Total Critical for {resultClusterName}->{resultNamespaceName}->{resultWorkloadType}->{resultWorkloadName}->{resultContainerName}: {severityCount}{RESET}")
+    
             if(progressCounter % 100 == 0):
                 print(f"{GREEN}Processing runtime assets {progressCounter} of {totalRuntimeFindings}...{RESET}")
 
+    print(f"{BLUE}Total assets scanned: {totalRuntimeFindings}{RESET}")
     print(f"{BLUE}Total runtime report entries: {originalTotalEntries}{RESET}")
     print(f"{BLUE}Total entries for final report: {newTotalEntries}{RESET}")
     print(f"{BLUE}Total inactive runtime entries trimmed {originalTotalEntries - newTotalEntries}{RESET}")
-    print(f"{BLUE}Total assets scanned: {totalRuntimeFindings}{RESET}")
     print(f"{GREEN}Output report filename: {outputFilename}{RESET}")
     
